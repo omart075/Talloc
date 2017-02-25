a3e65c1014ec483d69b9d3f6449653347b5c0eb6
omart075 <omart075@fiu.edu>
2017-02-25 05:27:45 -0500

    Retrieval of comments

--- background.js
+++ background.js
@@ -1,1 +1,19 @@
 //background.js
+
+//listens for the script to send message
+chrome.runtime.onMessage.addListener(
+  function(request, sender, callback){
+      var xhttp = new XMLHttpRequest();
+      var method = request.method;
+
+      //does request
+      xhttp.open(method, request.url, true);
+      xhttp.send(request.data);
+      //when request has returned, send data to callback function
+      xhttp.onload = function(){
+        console.log(xhttp.responseText)
+        callback(xhttp.responseText);
+      }
+      return true
+  }
+);
--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// script.js 
+// script.js
+var videoUrl = document.location.href;
+var videoId = videoUrl.split("v=")[1];
+
+console.log(videoId);
+
+//send message to background to run http request to avoid cross domain policy
+//callback function returns comments
+chrome.runtime.sendMessage({
+  method: 'GET',
+  url:'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=' + videoId + '&key=AIzaSyDvt7MmIIHpp3p_HP-pvU26bsZlaQhfR5s',
+  data: ''
+}, function(response){
+  console.log(response);
+});
