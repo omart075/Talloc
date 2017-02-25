1fd738e7ead168588e6f1f6028903b4c90b1126a
Christian Canizares <chris@7831c1ec3c6a.p.fiu.edu>
2017-02-25 17:19:04 -0500

    Timestamping

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 // script.js
 
-var timeout = null;
-document.addEventListener("DOMSubtreeModified",
-function()
-{
-	if(timeout)clearTimeout(timeout);
-	timeout = setTimeout(listener, 1000);
-}, false);
 
 
-function listener()
-{
-  console.log("here");
-  console.log(document.getElementsByClassName("ytp-progress-bar-padding")[0].offsetWidth);
-}
-
+var vidWidthPx = 0;
 
 var videoUrl = document.location.href;
 var videoId = videoUrl.split("v=")[1];
 
-console.log(videoId);
+//console.log(videoId);
 
 //send message to background to run http request to avoid cross domain policy
 //callback function returns comments
@@ -57,22 +45,67 @@ chrome.runtime.sendMessage({
 
         // The result can be accessed through the `m`-variable.
         m.forEach((match, groupIndex) => {
-        temp.push(`Found match, group ${groupIndex}: ${match}`);
+        temp.push(match);
         });
 
       }
 
     }
-    console.log(tracklist);
+    //console.log(tracklist);
+    function generateTimeStamps(){
+    console.log(tracklist[6])
+    for(var i = 0; i < tracklist.length; i++){
+    vidWidthPx = document.getElementsByClassName("ytp-progress-bar-padding")[0].offsetWidth;
 
+    var timeStampPre = Number(tracklist[i].split(":")[0]);
 
+    var timeStampPost = Number(tracklist[i].split(":")[1].split(" ")[0]);
 
+    var timeStampInSec = timeStampPre * 60 + Number(timeStampPost);
+
+    var vidLength = document.getElementsByClassName("ytp-time-duration")[0].innerHTML;
+
+    var vidLengthInSec = vidLength.split(":")[0] * 3600 + vidLength.split(":")[1] * 60 + Number(vidLength.split(":")[2]);
+
+    console.log(vidLengthInSec + " " + vidWidthPx);
+    var secPerPx = vidLengthInSec / vidWidthPx;
+    console.log(timeStampInSec + " " + secPerPx);
+    var timeStampPos = (timeStampInSec / secPerPx) - 1.5;
+    console.log(timeStampPos)
     //console.log(document.getElementsByClassName("ytp-progress-bar-padding")[0].offsetWidth);
     var a = document.createElement("div");
-    a.setAttribute("id", "square");
-    a.setAttribute("style", "width:10px; height:5px; background:yellow; left: 0px; transform: translateX(50%);");
+    a.setAttribute("id", "square" + i);
+    a.setAttribute("style", "width:5px; height:2.5px; background:orange; float: left; transform: translateX(" + timeStampPos + "px);");
     document.getElementsByClassName("ytp-progress-bar")[0].appendChild(a);
 
+
+}
+}
+
+function removeTimeStamps()
+{
+  for (var i=0; i < tracklist.length; i++)
+  {
+    try{
+    var element = document.getElementById("square" + i);
+    element.parentNode.removeChild(element);
+  } catch(err){break;}
+  }
+}
+var timeout = null;
+document.addEventListener("DOMSubtreeModified",
+function()
+{
+	if(timeout)clearTimeout(timeout);
+	timeout = setTimeout(listener, 1500);
+}, false);
+
+
+function listener()
+{
+}
+
+generateTimeStamps();
 
 
 
